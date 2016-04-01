98fa0655976cbf09a4d0ffcb366837ded107d3a3
Art Vandelay <westdorp@gmail.com>
2016-04-01 20:59:54 +0000

    fixes to wtforms

--- author/views.py
+++ author/views.py
@@ -9,4 +9,10 @@ def login():
 @app.route('/register', methods=('GET', 'POST'))
 def register():
     form = RegisterForm()
+    if form.validate_on_submit():
+        return redirect('/success')
     return render_template('author/register.html', form=form)
+
+@app.route('/success')
+def success():
+    return "Author registered!"
